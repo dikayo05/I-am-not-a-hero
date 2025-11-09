68dfa3401d00801ca1db065d73ea195dc7930585
Dika yonanda putra <yonandaputra05@gmail.com>
2025-11-09 09:57:50 +0700

    update the code according to the documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,47 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "I am not a hero");
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "I am not a hero");
     window.setFramerateLimit(30);
 
+    // Load a sprite to display
+    const sf::Texture texture("cute_image.jpg");
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    const sf::Font font("arial.ttf");
+    sf::Text text(font, "Hello SFML", 50);
+
+    // Load a music to play
+    sf::Music music("nice_music.ogg");
+
+    // Play the music
+    music.play();
+
+    // Start the game loop
     while (window.isOpen())
     {
+        // Process events
         while (const std::optional event = window.pollEvent())
         {
+            // Close window: exit
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
+        // Clear screen
         window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
         window.display();
     }
 }
